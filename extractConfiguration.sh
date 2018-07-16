9107996d1d1c9367d72dbc3f5d762bf00af1c3ae
Eduardo Patrocinio <epatro@gmail.com>
2018-07-16 08:13:55 -0300

    Fix extractConfiguration

--- extractConfiguration.sh
+++ extractConfiguration.sh
@@ -1,6 +1,7 @@
 export CLUSTER_DIR=/opt/icp-install/cluster
 export ICP_VERSION=2.1.0.3
 
+mkdir $CLUSTER_DIR
 cd $CLUSTER_DIR
 docker run -v $(pwd):/data -e LICENSE=accept \
  ibmcom/icp-inception:$ICP_VERSION-ee \
