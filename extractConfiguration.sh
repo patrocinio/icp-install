e26f3a09a02108cee8b0c66ffdc71b14604eb60a
Eduardo Patrocinio <epatro@gmail.com>
2018-07-16 08:00:19 -0300

    Add extractConfiguration

---
+++ extractConfiguration.sh
@@ -0,0 +1,7 @@
+export CLUSTER_DIR=/opt/icp-install/cluster
+export ICP_VERSION=2.1.0.3
+
+cd $CLUSTER_DIR
+docker run -v $(pwd):/data -e LICENSE=accept \
+ ibmcom/icp-inception:$ICP_VERSION-ee \
+ cp -r cluster /data
