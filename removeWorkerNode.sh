0f19bf46e8d2141208cfcc88a22c0e23e8069ddb
Eduardo Patrocinio <epatro@gmail.com>
2018-08-21 07:43:02 -0400

    Create removeWorkerNode

---
+++ removeWorkerNode.sh
@@ -0,0 +1,11 @@
+export NOW=$(date +"%Y-%m-%d_%H:%M:%S")
+export LOGFILE=/tmp/addnode-$NOW.log
+export CLUSTER_DIR=/opt/icp-install/cluster
+export ICP_VERSION=2.1.0.3
+NODE=$1
+
+cd $CLUSTER_DIR
+echo Adding node $NODE
+sudo docker run -e LICENSE=accept --net=host --rm -t -v "$(pwd)":/installer/cluster \
+	ibmcom/icp-inception:$ICP_VERSION-ee uninstall -l $NODE -vv | tee $LOGFILE
+
