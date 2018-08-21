7cc6ed9e660211b02fa4299d8cca04e2579b1bbf
Eduardo Patrocinio <epatro@gmail.com>
2018-08-21 07:22:42 -0400

    Fix add worker node

--- addWorkerNode.sh
+++ addWorkerNode.sh
@@ -6,5 +6,6 @@ NODE=$1
 
 cd $CLUSTER_DIR
 echo Adding node $NODE
-sudo docker run -e LICENSE=accept --net=host --rm -t -v "$(pwd)":/installer/cluster ibmcom/icp-inception:$ICP_VERSION-ee add node $NODE -vv | tee $LOGFILE
+sudo docker run -e LICENSE=accept --net=host --rm -t -v "$(pwd)":/installer/cluster \
+	ibmcom/icp-inception:$ICP_VERSION-ee worker $NODE -vv | tee $LOGFILE
 
