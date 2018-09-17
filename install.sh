b28a7229a71a18bec6eb81095f3990405d10867e
Eduardo Patrocinio <epatro@gmail.com>
2018-09-17 17:02:13 -0400

    Switch to /opt/ibm

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 export NOW=$(date +"%Y-%m-%d_%H:%M:%S")
 export LOGFILE=/tmp/install-$NOW.log
-export CLUSTER_DIR=/opt/icp-install/cluster
+export CLUSTER_DIR=/opt/ibm/cluster
 export ICP_VERSION=2.1.0.3
 cd $CLUSTER_DIR
 sudo docker run -e LICENSE=accept --net=host --rm -t -v "$(pwd)":/installer/cluster ibmcom/icp-inception:$ICP_VERSION-ee install -vvv | tee $LOGFILE
