c4e1dee386a298fe6b3097ccaad60c7d813960ea
Eduardo Patrocinio <epatro@gmail.com>
2018-11-04 14:34:41 -0200

    Configure for ICP 3.1

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 export NOW=$(date +"%Y-%m-%d_%H:%M:%S")
 export LOGFILE=/tmp/uninstall-$NOW.log
 export CLUSTER_DIR=/opt/ibm/cluster
-export ICP_VERSION=2.1.0.3
+export ICP_VERSION=3.1.0
 cd $CLUSTER_DIR
-sudo docker run -e LICENSE=accept --net=host --rm --name=installer -t -v $(pwd):/installer/cluster ibmcom/icp-inception:$ICP_VERSION-ee uninstall  -vvv | tee $LOGFILE
+sudo docker run -e LICENSE=accept --net=host --rm --name=installer -t -v $(pwd):/installer/cluster ibmcom/icp-inception:$ICP_VERSION uninstall  -vvv | tee $LOGFILE
 
